7abae1c1faf28b7d8240c9fa94f2f8d1ffcecf4f
Ujjwal gupta <ugupta17k@gmail.com>
2026-01-16 03:06:32 +0530

    integrated Ai

--- server.js
+++ server.js
@@ -11,3 +11,4 @@ initSocketServer(httpServer);
 httpServer.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,10 +1,49 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const userModel = require('../models/user.model')
+const jwt = require("jsonwebtoken")
+const aiService = require("../services/ai.service")
+
 
 function initSocketServer(httpServer){
     const io = new Server(httpServer, {} )
-    io.on("connection", (socket)=>{
-        console.log("New socket connection:", socket.id);
+
+    io.use(async(socket,next)=>{
+        const cookies = cookie.parse(socket.handshake.headers?.cookie ||"")
+        console.log("Socket connection cookie", cookies)
+
+        if(!cookies.token){
+            next(new Error("Authentication error: No token provided"))
+        }
+
+        try{
+            const decoded = jwt.verify(cookies.token , process.env.JWT_SECRET);
+            const user = await userModel.findById(decoded.id)
+            socket.user = user
+            next();
+
+        }catch(err){
+            next(new Error("Authentication error : Invalid token"))
+        }
+
     })
+
+    io.on("connection", (socket)=>{
+
+
+        socket.on("ai-message" , async(messagePayload)=>{
+
+            const response = await aiService.generateResponse(messagePayload.content)
+
+            socket.emit("ai-response" , {
+                content: response,
+                chat : messagePayload.chat
+            })
+
+        })
+
+    })
+    
 }
 
 module.exports = initSocketServer ;
